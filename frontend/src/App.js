0b31a856e081744a53af92bd97e8a68c55f9e495
Gagandeep-Kaur-01 <gagandeep07kaur@gmail.com>
2021-03-07 21:33:58 +0530

    created react app and installed bootstrap

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import React,  {Component } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+class App extends Component {
+  render() {
+    return(
+      <div>
+        Hello
+      </div>
+    )
+
+  }
+}
+
+export default App;
