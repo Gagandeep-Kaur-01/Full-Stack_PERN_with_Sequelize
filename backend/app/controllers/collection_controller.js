6f6ead0c5d6e1a5d9ec82c4a2a50cde09bcf1c6b
Gagandeep-Kaur-01 <gagandeep07kaur@gmail.com>
2021-03-07 13:50:21 +0530

    created controller with CRUD functions

---
+++ backend/app/controllers/collection_controller.js
@@ -0,0 +1,38 @@
+const db = require("../models");
+const Collection = db.collections;
+const Op = db.Sequelize.Op;
+
+// Create and Save a new Collection
+exports.create = (req, res) => {
+  
+};
+
+// Retrieve all Collections from the database.
+exports.findAll = (req, res) => {
+  
+};
+
+// Find a single Collection with an id
+exports.findOne = (req, res) => {
+  
+};
+
+// Update a Collection by the id in the request
+exports.update = (req, res) => {
+  
+};
+
+// Delete a Collection with the specified id in the request
+exports.delete = (req, res) => {
+  
+};
+
+// Delete all Collections from the database.
+exports.deleteAll = (req, res) => {
+  
+};
+
+// Find all published Collections
+exports.findAllPublished = (req, res) => {
+  
+};
