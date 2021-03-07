e28be84ee09708c4bc4f094578bedc43b0f396e7
Gagandeep-Kaur-01 <gagandeep07kaur@gmail.com>
2021-03-07 21:39:44 +0530

    installed react-router-dom & wrapped App component by BrowserRouter

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import { BrowserRouter } from "react-router-dom";
+
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
