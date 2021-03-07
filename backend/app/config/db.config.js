10f573b7aee508d0b43260ed88f3bf63b4bc8783
Gagandeep-Kaur-01 <gagandeep07kaur@gmail.com>
2021-03-07 13:24:13 +0530

    added comments

--- backend/app/config/db.config.js
+++ backend/app/config/db.config.js
@@ -5,9 +5,9 @@ module.exports = {
     DB: "pern_s",
     dialect: "postgres",
     pool: {
-        max: 5,
-        min: 0,
-        acquire: 30000,
-        idle: 10000
+        max: 5, // maximum number of connection in pool
+        min: 0, // minimum number of connection in pool
+        acquire: 30000, // maximum time, in milliseconds, that pool will try to get connection before throwing error
+        idle: 10000 // maximum time, in milliseconds, that a connection can be idle before being released
     }
 };
