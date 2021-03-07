687692303d520ae69badf096c4af0d65609b731d
Gagandeep-Kaur-01 <gagandeep07kaur@gmail.com>
2021-03-07 21:54:22 +0530

    added navbar, switch, routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,36 @@
 import React,  {Component } from "react";
+import { Link, Switch, Route } from 'react-router-dom';
 import "bootstrap/dist/css/bootstrap.min.css";
 
 class App extends Component {
   render() {
-    return(
+    return (
       <div>
-        Hello
-      </div>
-    )
+        <nav className="navbar navbar-expand navbar-dark bg-dark">
+          <a href="/collections" className="navbar-brand">
+            Awesome
+          </a>
+          <div className="navbar-nav mr-auto">
+            <li className="nav-item">
+              <Link to={"/collections"} className="nav-link">
+                Collections
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to={"/add"} className="nav-link">
+                Add
+              </Link>
+            </li>
+          </div>
+        </nav>
 
+        <div className="container mt-3">
+          <Switch>
+            
+          </Switch>
+        </div>
+      </div>
+    );
   }
 }
 
